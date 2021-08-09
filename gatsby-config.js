41b725148764d12c02131a3f2dbfc8cee1f111a1
Jomar <jomar.cardoso@cwi.com.br>
2021-08-08 23:05:44 -0300

    feat: 🎸 new logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
         background_color: `#f8f8f8`,
         theme_color: `#87695e`,
         display: `standalone`,
-        icon: `src/images/original6.png`,
+        icon: `src/images/logo.png`,
         cache_busting_mode: 'none',
       },
     },
