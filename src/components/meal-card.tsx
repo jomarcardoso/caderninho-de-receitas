6b03946269f28b04d689d708296104ffd599fb93
jomarcardoso <antonionetocardoso@gmail.com>
2021-08-14 10:31:11 -0300

    feat: 🎸 remove meal

--- src/components/meal-card.tsx
+++ src/components/meal-card.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, SyntheticEvent, useContext, useState } from 'react';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
@@ -7,10 +7,20 @@ import { Link } from 'gatsby';
 import Typography from '@material-ui/core/Typography';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+// import CardMedia from '@material-ui/core/CardMedia';
+import MenuItem from '@material-ui/core/MenuItem';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import Card from './card/card';
 import { Meal } from '../services/meal';
-import ResumedPortion from './resumed-portion';
+// import ResumedPortion from './resumed-portion';
 import { light, borderSecondary } from './layout/layout';
+import AccountContext from '../contexts/account-context';
 
 const useStyles = makeStyles({
   cardLink: {
@@ -27,10 +37,6 @@ const useStyles = makeStyles({
     flex: 1,
     backgroundColor: light,
   },
-  list: {
-    flexWrap: 'nowrap',
-    overflowX: 'auto',
-  },
 });
 
 interface Props {
@@ -38,54 +44,140 @@ interface Props {
 }
 
 const MealCard: FC<Props> = ({ meal }) => {
+  const { setAccount } = useContext(AccountContext);
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState<Element | null>();
 
-  const mainIngredients = meal.portions
-    .sort(
-      (portionBefore, portionCurrent) =>
-        portionCurrent.quantity - portionBefore.quantity,
-    )
-    .slice(0, 4);
+  function handleClick(event: SyntheticEvent) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
+  function handleClickRemove() {
+    setAccount?.removeMeal(meal.id);
+    handleClose();
+  }
+
+  const mainIngredients = meal.portions.sort(
+    (portionBefore, portionCurrent) =>
+      portionCurrent.quantity - portionBefore.quantity,
+  );
 
   return (
-    <Link to={`/meal#${meal.id}`} state={{ meal }} className={classes.cardLink}>
-      <Card className={classes.card}>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="recipe" src={meal.image} variant="rounded" />
-          }
-          color="textSecondary"
-          title={<Typography variant="h3">{meal.name}</Typography>}
-        />
-        <Box bgcolor="grey.300" className={classes.cardBody}>
-          <CardContent>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Grid container spacing={2} className={classes.list}>
-                  {mainIngredients.map((portion) => (
-                    <Grid item xs={3} key={portion.food.id}>
-                      <ResumedPortion padding={6} portion={portion} />
-                    </Grid>
-                  ))}
-                </Grid>
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Box>
-        <CardContent>
-          <Grid
-            container
-            spacing={2}
-            justifyContent="space-between"
-            alignItems="flex-end"
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" src={meal.image} variant="rounded" />
+        }
+        color="textSecondary"
+        title={
+          <Link
+            to={`/meal#${meal.id}`}
+            state={{ meal }}
+            className={classes.cardLink}
           >
+            <Typography variant="h3">{meal.name}</Typography>
+          </Link>
+        }
+        action={
+          <div>
+            <Button
+              aria-owns={anchorEl ? 'simple-menu' : undefined}
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem>
+                <Link
+                  to={`/meal#${meal.id}`}
+                  state={{ meal }}
+                  className={classes.cardLink}
+                >
+                  Ver receita
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleClickRemove}>remover</MenuItem>
+            </Menu>
+          </div>
+        }
+      />
+      <CardContent>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography>{meal.description}</Typography>
+          </Grid>
+          {/* <Grid item xs={12}>
+            <Grid container spacing={2}>
+              {mainIngredients.map((portion) => (
+                <Grid item xs={3} key={portion.food.id}>
+                  <ResumedPortion padding={6} portion={portion} />
+                </Grid>
+              ))}
+            </Grid>
+          </Grid> */}
+        </Grid>
+      </CardContent>
+      <Box bgcolor="grey.300" className={classes.cardBody}>
+        <CardContent>
+          <Grid container spacing={2}>
+            {/* <Grid item xs={12}>
+              <Typography>{meal.description}</Typography>
+            </Grid> */}
             <Grid item xs={12}>
-              <Typography variant="h4">Calorias: {meal.calories}</Typography>
+              {/* <Grid container spacing={2}>
+                {mainIngredients.map((portion) => (
+                  <Grid item xs={3} key={portion.food.id}>
+                    <ResumedPortion padding={6} portion={portion} />
+                  </Grid>
+                ))}
+              </Grid> */}
+              <Typography component="h3" variant="h3">
+                Ingredientes
+              </Typography>
+              <List dense>
+                {mainIngredients.map((portion) => (
+                  <ListItem>
+                    <ListItemText>{portion.description}</ListItemText>
+                  </ListItem>
+                ))}
+              </List>
+              {meal.preparation && (
+                <>
+                  <Typography component="h3" variant="h3">
+                    Modo de preparo
+                  </Typography>
+                  <Typography>{meal.preparation}</Typography>
+                </>
+              )}
             </Grid>
           </Grid>
         </CardContent>
-      </Card>
-    </Link>
+      </Box>
+      <CardContent>
+        <Grid
+          container
+          spacing={2}
+          justifyContent="space-between"
+          alignItems="flex-end"
+        >
+          <Grid item xs={12}>
+            <Typography variant="h4">Calorias: {meal.calories}</Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 };
 
