b45747579c4db49308ecf481bc7918a9baf747ee
jomarcardoso <antonionetocardoso@gmail.com>
2021-08-21 15:49:55 -0300

    fix: 🐛 lint

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,7 +3,6 @@ import Box from '@material-ui/core/Box';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Header, { HeaderProps } from '../header';
 import Main from '../main';
-import Footer from '../footer';
 import './layout.scss';
 
 import { CurrentPage } from '../../services/page.service';
