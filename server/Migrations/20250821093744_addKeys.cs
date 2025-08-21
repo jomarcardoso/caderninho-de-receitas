e5b402319ae1f90caf7c66d6b1842e0a1d8520cd
jomarcardoso <antonionetocardoso@gmail.com>
2025-08-21 17:38:55 +0800

    feat(server): add original keys in english

---
+++ server/Migrations/20250821093744_addKeys.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class addKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Keys",
+                table: "Foods",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Keys",
+                table: "Foods");
+        }
+    }
+}
