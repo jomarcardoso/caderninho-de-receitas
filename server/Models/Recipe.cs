45888838cc74d9e36b0e199cb2885ce619d1d374
jomarcardoso <antonionetocardoso@gmail.com>
2025-08-26 21:35:45 +0800

    fix(server): rename recipe id to Id

--- server/Models/Recipe.cs
+++ server/Models/Recipe.cs
@@ -20,7 +20,7 @@
 
     public class Recipe
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public string? Additional { get; set; }
